5145969493624d7cf39d56b72a91119cdbb1c532
Partha <partha.abdas@gmail.com>
2023-05-02 05:25:21 -0500

    developer apply

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,25 @@
-## Security group
-locals {
-  az_1a            = var.availability_zone_1a
-  public_subnet_1a = var.public_subnet_1a
+## Provider
+provider "aws" {
+  region = var.region
 }
 
+## Terraform version
+terraform {
+  required_version = ">= 0.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.5"
+    }
+  }
+}
+
+# terraform state backe
+terraform {
+  backend "s3" {
+  }
+}
 # This function will get the latest ami id from AMI list
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -22,13 +38,13 @@ data "aws_ami" "ubuntu" {
 # This block will create the aws ec2 instance
 resource "aws_instance" "devsecops-public-ec2" {
   ami                         = data.aws_ami.ubuntu.id
-  availability_zone           = local.az_1a
+  availability_zone           = var.availability_zone_1a
   instance_type               = var.instance_type
   associate_public_ip_address = true
   key_name                    = var.instance_keypair
   subnet_id                   = var.public_subnet_1a
   security_groups             = flatten([var.security_groups_ids])
-  user_data                   = file("files/server_config.sh")
+  user_data                   = file("scripts/server_config.sh")
   tenancy                     = "default"
 
   tags = {
